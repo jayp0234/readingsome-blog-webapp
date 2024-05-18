ad6927f53d2e8a2adcce1a650973a0d366529bec
Jay Patel <jayu.patel0234@gmail.com>
2024-05-18 07:49:48 -0600

    SEO updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,6 +50,15 @@ const App = () => {
   return (
     <ThemeContext.Provider value={{theme, setTheme}}>
     <UserContext.Provider value={{ userAuth, setUserAuth }}>
+    <Helmet>
+          <title>ReadingSome</title>
+          <meta name="description" content="Reading Some - A platform to discover and share interesting articles, blogs, and stories." />
+          <meta name="keywords" content="Reading, Articles, Blogs, Stories, Discover, Share, Comparision, readingsome, ReadingSome" />
+          <meta name="application-name" content="Reading Some" />
+          <meta name="apple-mobile-web-app-title" content="Reading Some" />
+          <meta name="robots" content="index, follow" />
+          <meta name="googlebot" content="index, follow" />
+        </Helmet>
       <Routes>
         <Route path="/editor" element={<Editor />} />
         <Route path="/editor/:blog_id" element={<Editor />} />
